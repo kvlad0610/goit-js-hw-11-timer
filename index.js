055808b6137336fcb308b645aeece3c471370f66
Vlad <kvlad0610@gmail.com>
2020-12-19 04:13:41 +0200

    Щетчик до НГ

---
+++ index.js
@@ -0,0 +1,29 @@
+const refs = {
+  days: document.querySelector('span[data-value="days"]'),
+  hours: document.querySelector('span[data-value="hours"]'),
+  mins: document.querySelector('span[data-value="mins"]'),
+  secs: document.querySelector('span[data-value="secs"]'),
+};
+
+const targetDate = new Date("Jan 01, 2021");
+const countdownTimer = {
+  start() {
+    setInterval(() => {
+      const deltaTime = targetDate.getTime() - Date.now();
+      updateClockFace(deltaTime);
+    }, 1000);
+  },
+};
+
+countdownTimer.start();
+
+function updateClockFace(time) {
+  const days = Math.floor(time / (1000 * 60 * 60 * 24));
+  const hours = Math.floor((time % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+  const mins = Math.floor((time % (1000 * 60 * 60)) / (1000 * 60));
+  const secs = Math.floor((time % (1000 * 60)) / 1000);
+  refs.days.textContent = `${days}`;
+  refs.hours.textContent = `${hours}`;
+  refs.mins.textContent = `${mins}`;
+  refs.secs.textContent = `${secs}`;
+}
